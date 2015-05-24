4cac8aa237c26e31e405751eb58f87ff5ca575e9
Jorge Atempa <atempaitm@gmail.com>
2015-05-24 16:57:22 -0700

    Se agregaron dos elementos nuevos al controlador

--- js/controllers/barCtrl.js
+++ js/controllers/barCtrl.js
@@ -1,7 +1,19 @@
 app.controller('barCtrl', ['$scope', function ($scope) {
-	this.datos = {
-		nombre: "Blu",
-		slogan: "Nigth Club",
-		logotipo: "img/Blu.jpg"
-	}
+	this.datos = [
+		{
+			nombre: "Blu",
+			slogan: "Nigth Club",
+			logotipo: "img/Blu.jpg"
+		},
+		{
+            nombre: "Hijole",
+            slogan: "Cantina & Bar",
+            logotipo: 'img/Hijole.jpg'
+        },
+        {
+            nombre: "Los Rieles",
+            slogan: "Antro & Banda",
+            logotipo: 'img/Rieles.jpg'
+        }
+	]
 }])
